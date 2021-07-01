54de65fe703ca65c667bec5f1beaf0df8c36ed1f
alice_just <alicejust53@gmail.com>
2021-07-01 08:32:25 +0300

    Change methods totalCost and getPrice

--- src/main/java/goit/homework/shop/ShopStorage.java
+++ src/main/java/goit/homework/shop/ShopStorage.java
@@ -18,20 +18,20 @@ public class ShopStorage {
         productsBucket.put("D", new ProductStorageImpl("D", 0.75));
     }
 
-   private String filteredBucket(String bucket) {
+    private String filteredBucket(String bucket) {
         if (bucket != null && !bucket.isEmpty()) {
             return Arrays.stream(bucket.toUpperCase()
                     .split(""))
                     .filter(productsBucket::containsKey)
                     .collect(Collectors.joining());
         } else {
-            throw new NullPointerException("Empty bucket, try again");
+            throw new NullPointerException("You write wrong product id, check it and try again");
         }
     }
 
-    public  double calculateTotalCost(String bucket) {
+    public double calculateTotalCost(String bucket) {
+       String customerBucket = filteredBucket(bucket);
         if (bucket != null && !bucket.isEmpty()) {
-            String customerBucket = filteredBucket(bucket);
             Map<String, Long> productsQuantity = Arrays.stream(customerBucket.split(""))
                     .map(productsBucket::get)
                     .collect(Collectors.groupingBy(ProductStorageImpl::getId,
@@ -40,10 +40,11 @@ public class ShopStorage {
                     .mapToDouble(p -> productsBucket.get(p.getKey()).getProductPrice((p.getValue())))
                     .sum();
         } else {
-            throw new NullPointerException("Empty bucket, try again");
+            return 0;
         }
     }
 }
+
 
 
 
